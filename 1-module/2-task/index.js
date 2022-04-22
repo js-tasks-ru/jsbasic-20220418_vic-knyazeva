6741fa6d53ca1811a700590a1cab94a1b860a84f
VicKnyazeva <victoria.knyazeva@hotmail.com>
2022-04-23 01:13:46 +0700

    feat: 1-module/tasks 3,4,5

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,10 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if(!name)
+    return false;
+
+  return (!name.includes(' ') && name.length >= 4);
 }
 
 function sayHello() {
